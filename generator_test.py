272f4aabc7cc16124d388430eddffb32b725dec6
molano <mmolano0@hotmail.com>
2017-03-26 12:08:04 +0200

    use nn_resize (see previous commit)

--- generator_test.py
+++ generator_test.py
@@ -11,14 +11,13 @@ class batch_norm(object):
       self.name = name
 
   def __call__(self, x, train=True):
-     return x
-#    return tf.contrib.layers.batch_norm(x,
-#                      decay=self.momentum, 
-#                      updates_collections=None,
-#                      epsilon=self.epsilon,
-#                      scale=True,
-#                      is_training=train,
-#                      scope=self.name)
+    return tf.contrib.layers.batch_norm(x,
+                      decay=self.momentum, 
+                      updates_collections=None,
+                      epsilon=self.epsilon,
+                      scale=True,
+                      is_training=train,
+                      scope=self.name)
 
 def conv_cond_concat(x, y):
   """Concatenate conditioning vector on feature map axis."""
@@ -31,6 +30,8 @@ def deconv1d(input_, output_shape,
        name="deconv2d"):
   with tf.variable_scope(name):
     # filter : [height, width, output_channels, in_channels]
+    print('kernel size')
+    print([k_h, 1, output_shape[-1], input_.get_shape()[-1]])
     w = tf.get_variable('w', [k_h, 1, output_shape[-1], input_.get_shape()[-1]],
               initializer=tf.random_normal_initializer(stddev=stddev))
     
@@ -42,6 +43,36 @@ def deconv1d(input_, output_shape,
     deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
    
     return deconv[:,:,0,:]
+
+
+def nn_resize(input_, output_shape,
+       k_h=5, d_h=1, stddev=0.02,
+       name="deconv2d"):
+  with tf.variable_scope(name):
+    # filter : [height, width, output_channels, in_channels]
+    w = tf.get_variable('w', [k_h, input_.get_shape()[-1], output_shape[-1]],
+              initializer=tf.truncated_normal_initializer(stddev=stddev))   
+    print('ooooooooooooooooooooooooooooooooooooo')
+    print('kernel size')
+    print(w.get_shape())
+    aux_input = tf.expand_dims(input_,axis=2)
+    print('input size')
+    print(input_.get_shape())
+    print('aux input size')
+    print(aux_input.get_shape())
+    resized_image = tf.image.resize_images(aux_input, output_shape[1:3], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    resized_image = resized_image[:,:,0,:]
+    print('resized image shape')
+    print(resized_image.get_shape())
+    deconv = tf.nn.conv1d(resized_image, w, stride=d_h, padding='SAME')
+    print('deconv size')
+    print(deconv.get_shape())
+    print('ooooooooooooooooooooooooooooooooooooo')
+    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+    deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+   
+    return deconv
+
      
 def lrelu(x, leak=0.2, name="lrelu"):
   return tf.maximum(x, leak*x)
@@ -111,7 +142,7 @@ print(h1.get_shape())
 h1 = conv_cond_concat(h1, yb)
 
 #third layer (deconv2d + batch norm. + relu)
-h2_deconv = deconv1d(h1,[batch_size, s_h2, 1, gf_dim ], name='g_h2')
+h2_deconv = nn_resize(h1,[batch_size, s_h2, 1, gf_dim ], name='g_h2')
 h2_norm = g_bn2(h2_deconv)
 h2_activity = tf.nn.relu(h2_norm)
 print('h2.get_shape()')
@@ -119,7 +150,7 @@ print(h2_activity.get_shape())
 h2 = conv_cond_concat(h2_activity, yb)
 
 #third layer (deconv2d + signmoid, no batch norm.)
-h3_deconv = deconv1d(h2, [batch_size, s_h, 1, output_depth], name='g_h3')
+h3_deconv = nn_resize(h2, [batch_size, s_h, 1, output_depth], name='g_h3')
 h3_activity = tf.nn.sigmoid(h3_deconv)
 print('h3.get_shape()')
 print(h3_activity.get_shape())
@@ -148,7 +179,7 @@ sbplt[2][0].plot(np.transpose(samples_plot.eval()))
 sbplt[2][0].set_title('h2 deconv')
 samples_plot = h2_norm[:,:,0]
 sbplt[2][1].plot(np.transpose(samples_plot.eval()))
-sbplt[2][1].set_title('h1 norm')
+sbplt[2][1].set_title('h2 norm')
 samples_plot = h2_activity[:,:,0]
 sbplt[2][2].plot(np.transpose(samples_plot.eval()))
 sbplt[2][2].set_title('h2 activity')

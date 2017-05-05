12735df993b64405d34f58c369b47fdcc8b38801
molano <mmolano0@hotmail.com>
2017-05-05 15:41:56 +0200

    add back the for loop over neurons
    the idea was to test whether the sum of all neurons activities
    would be a better candidate to feed to the GAN (individual neurons
    are not very active)

--- dataprovider.py
+++ dataprovider.py
@@ -101,7 +101,7 @@ def generate_spike_trains(parameters):
     elif parameters.dataset=='retinal_data':
         if refr_per!=-1:    
             raise ValueError("Applying refractory period to retinal data!")
-        X,_ = load_spikes('/home/manuel/DATA/data/Scales/RetinalData/NeuralData/Spikes/','Movie2Exp1',1, num_bins, parameters.neuron)        
+        X,_ = load_spikes('/home/manuel/DATA/data/Scales/RetinalData/NeuralData/Spikes/','Movie2Exp1',1, num_bins)        
         X = np.expand_dims(X,axis=2)
         X[X>0] = 1
         y = np.ones((num_samples, 1))
@@ -227,7 +227,7 @@ def refractory_period_control(refr_per, r, firing_rate):
     return r
 
 
-def load_spikes(folder, movie, bin_size, num_bins, neuron):
+def load_spikes(folder, movie, bin_size, num_bins):
     #load
     mat_contents = sio.loadmat(folder + movie + '.mat')
     #create figures folder
@@ -238,44 +238,44 @@ def load_spikes(folder, movie, bin_size, num_bins, neuron):
     #get spikes
     spks = mat_contents['Spikes']
     
-    spks = spks[neuron,:]
-    print(np.shape(spks))
     duration = 0
     
     size_mat = spks.shape
+    num_neurons = size_mat[0]
     
-    num_movie_repetitions = size_mat[0]
+    num_movie_repetitions = size_mat[1]
     
     #find maximum spiketime that will be assumed to be the duration of one single movie repetition
     #(since trials have different numbers of spikes the function np.max is not able to compute the maximum)
-    for ind_trial in range(num_movie_repetitions):
-        trial = spks[ind_trial]
-        if trial.size !=0:
-            trial_max = trial.max()
-            duration = max([trial_max,duration])
+    for ind_neuron in range(num_neurons):
+        for ind_trial in range(num_movie_repetitions):
+            trial = spks[ind_neuron][ind_trial]
+            if trial.size !=0:
+                trial_max = trial.max()
+                duration = max([trial_max,duration])
         
     
     num_trials_per_movie_repetition = int(duration/(num_bins*bin_size))
     num_trials = int(num_trials_per_movie_repetition*num_movie_repetitions)
     
-    binned_mat = np.zeros((num_trials, num_bins))
+    binned_mat = np.zeros((num_neurons,num_trials, num_bins))
     maximo = 0
     minimo = 100000
+    for ind_neuron in range(num_neurons):
+        contador = 0
+        for ind_trial in range(num_movie_repetitions):
+            trial_spks = spks[ind_neuron][ind_trial]
+            for ind_w in range(num_trials_per_movie_repetition):
+                window_spks = trial_spks[((ind_w*bin_size*num_bins)<=trial_spks) & (((ind_w+1)*bin_size*num_bins)>trial_spks)] - ind_w*bin_size*num_bins
+                
+                if window_spks.size !=0:
+                    maximo = np.max([maximo,np.max(window_spks)])
+                    minimo = np.min([minimo,np.min(window_spks)])
+                    num_spks = window_spks.size
+                    for ind_spks in range(num_spks):
+                        binned_mat[ind_neuron][contador][int(np.floor((window_spks[ind_spks])/bin_size))] = \
+                        binned_mat[ind_neuron][contador][int(np.floor((window_spks[ind_spks])/bin_size))] + 1
+                contador += 1    
     
-    contador = 0
-    for ind_trial in range(num_movie_repetitions):
-        trial_spks = spks[ind_trial]
-        for ind_w in range(num_trials_per_movie_repetition):
-            window_spks = trial_spks[((ind_w*bin_size*num_bins)<=trial_spks) & (((ind_w+1)*bin_size*num_bins)>trial_spks)] - ind_w*bin_size*num_bins
-            if window_spks.size !=0:
-                maximo = np.max([maximo,np.max(window_spks)])
-                minimo = np.min([minimo,np.min(window_spks)])
-                num_spks = window_spks.size
-                for ind_spks in range(num_spks):
-                    binned_mat[contador][int(np.floor((window_spks[ind_spks])/bin_size))] = \
-                    binned_mat[contador][int(np.floor((window_spks[ind_spks])/bin_size))] + 1
-            contador += 1    
-    
-    
-    
+    binned_mat = np.mean(binned_mat,axis=0)
     return binned_mat,spks

4babbaa4a8613746b0c0af7adf6a23af6e427ffc
molano <mmolano0@hotmail.com>
2017-05-07 19:20:52 +0200

    funct to produce figs, call DCGAN to get sampls
    the function runs the DCGAN with the best configuration found
     among the different simulations/training steps.

--- compare_training_sessions.py
+++ compare_training_sessions.py
@@ -14,6 +14,57 @@ os.chdir(main_folder)
 import utils
 
 
+def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp):
+    folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
+  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
+
+    title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
+    best_fit_ac,best_fit_prob = utils.compare_trainings(main_folder+folder,title)
+    fig = plt.figure(figsize=(8,8),dpi=250)
+    img = mpimg.imread(main_folder+folder+'/training_error.png')
+    plt.imshow(img)
+    pp.savefig(fig)
+    plt.close()
+    
+    #plot best ac fit
+    fig = utils.plot_best_fit(best_fit_ac,folder+'/best_of_all_ac_fit')
+    pp.savefig(fig)
+    plt.close()
+    #want to get samples from the best ac fit
+    iteration = best_fit_ac['folder'][best_fit_ac['folder'].find('iteration')+10:]
+    training_stage = int(int(best_fit_ac['epoch'])*num_samples/64+int(best_fit_ac['step'])+1)
+    print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+              + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+    os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+                    + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+    
+    fig = plt.figure(figsize=(8,8),dpi=250)
+    img = mpimg.imread(best_fit_ac['folder']+'/fake_samples_binarized.png')
+    plt.imshow(img)
+    pp.savefig(fig)
+    plt.close()
+    
+    
+    
+    
+    fig = utils.plot_best_fit(best_fit_prob,folder+'/best_of_all_prob_fit')
+    pp.savefig(fig)
+    plt.close()
+    #want to get samples from the best ac fit
+    iteration = best_fit_prob['folder'][best_fit_prob['folder'].find('iteration')+10:]
+    training_stage = int(int(best_fit_prob['epoch'])*num_samples/64+int(best_fit_prob['step'])+1)
+    print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+              + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+    os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+                    + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+    
+    fig = plt.figure(figsize=(8,8),dpi=250)
+    img = mpimg.imread(best_fit_prob['folder']+'/fake_samples_binarized.png')
+    plt.imshow(img)
+    pp.savefig(fig)
+    plt.close()
+        
+
     
 pp = PdfPages(main_folder+'/training_error_summary.pdf')
 num_bins = 28
@@ -24,26 +75,7 @@ num_classes = 1
 classes_proportion = 'equal' 
 ref_period = -1
 num_samples = 16384
-
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
 
 # refractory period = 2
 dataset = 'uniform_fr'
@@ -51,26 +83,8 @@ num_classes = 1
 classes_proportion = 'equal' 
 ref_period = 2
 num_samples = 16384
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-  
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+
 
 # GAUSSIAN FIRING RATES
 # no refractory period
@@ -79,26 +93,8 @@ num_classes = 1
 classes_proportion = 'equal' 
 ref_period = -1
 num_samples = 16384
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-  
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+
 
 # refractory period = 2
 dataset = 'gaussian_fr'
@@ -106,26 +102,8 @@ num_classes = 1
 classes_proportion = 'equal' 
 ref_period = 2
 num_samples = 16384
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-  
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+
 
 # 2 classes / refractory period = -1
 dataset = 'gaussian_fr'
@@ -133,26 +111,7 @@ num_classes = 2
 classes_proportion = 'equal' 
 ref_period = -1
 num_samples = 16384
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-  
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
 
 # 2 classes / refractory period = 2
 dataset = 'gaussian_fr'
@@ -160,26 +119,7 @@ num_classes = 2
 classes_proportion = 'equal' 
 ref_period = 2
 num_samples = 16384
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-  
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
 
 # 2 classes / refractory period = -1 / classes proportion 70-30
 dataset = 'gaussian_fr'
@@ -187,51 +127,15 @@ num_classes = 2
 classes_proportion = '7030' 
 ref_period = -1
 num_samples = 16384
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-  
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+
 
 # 2 classes / refractory period = 2 / classes proportion 70-30
 num_classes = 2
 classes_proportion = '7030' 
 ref_period = 2
 num_samples = 16384
-folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
-  
-title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
-utils.compare_trainings(main_folder+folder,title)
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/training_error.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_ac_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()    
-fig = plt.figure(figsize=(8,8),dpi=250)
-img = mpimg.imread(main_folder+folder+'/best_of_all_prob_fit.png')
-imgplot = plt.imshow(img)
-pp.savefig(fig)
-plt.close()
+get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+
 
 pp.close()

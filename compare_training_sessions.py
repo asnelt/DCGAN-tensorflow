42c5de4cbf7709d512478982266658f3d8ee5281
molano <mmolano0@hotmail.com>
2017-05-09 15:55:57 +0200

    option for the different parameter configurations
    so the code does not run the analysis for all configurations
    everytime. Some other small changes.

--- compare_training_sessions.py
+++ compare_training_sessions.py
@@ -14,7 +14,7 @@ os.chdir(main_folder)
 import utils
 
 
-def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp):
+def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob):
     folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
   '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
 
@@ -26,116 +26,137 @@ def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,n
     pp.savefig(fig)
     plt.close()
     
+    
     #plot best ac fit
     fig = utils.plot_best_fit(best_fit_ac,folder+'/best_of_all_ac_fit')
     pp.savefig(fig)
     plt.close()
-    #want to get samples from the best ac fit
-    iteration = best_fit_ac['folder'][best_fit_ac['folder'].find('iteration')+10:]
-    training_stage = int(int(best_fit_ac['epoch'])*num_samples/64+int(best_fit_ac['step'])+1)
-    print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-              + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
-    os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-                    + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+    if ac_or_prob=='ac':
+        #now I want to get samples from the best ac fit
+        iteration = best_fit_ac['folder'][best_fit_ac['folder'].find('iteration')+10:]
+        training_stage = int(int(best_fit_ac['epoch'])*num_samples/64+int(best_fit_ac['step'])+1)
+        print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+                  + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+        os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+                        + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+     
+        fig = plt.figure(figsize=(8,8),dpi=250)
+        img = mpimg.imread(best_fit_ac['folder']+'/fake_samples_binarized.png')
+        plt.imshow(img)
+        pp.savefig(fig)
+        plt.close()
     
-    fig = plt.figure(figsize=(8,8),dpi=250)
-    img = mpimg.imread(best_fit_ac['folder']+'/fake_samples_binarized.png')
-    plt.imshow(img)
-    pp.savefig(fig)
-    plt.close()
     
-    
-    
-    
+       
+    #plot best prob fit
     fig = utils.plot_best_fit(best_fit_prob,folder+'/best_of_all_prob_fit')
     pp.savefig(fig)
     plt.close()
-    #want to get samples from the best ac fit
-    iteration = best_fit_prob['folder'][best_fit_prob['folder'].find('iteration')+10:]
-    training_stage = int(int(best_fit_prob['epoch'])*num_samples/64+int(best_fit_prob['step'])+1)
-    print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+    if ac_or_prob=='prob':   
+        #now I want to get samples from the best prob fit
+        iteration = best_fit_prob['folder'][best_fit_prob['folder'].find('iteration')+10:]
+        training_stage = int(int(best_fit_prob['epoch'])*num_samples/64+int(best_fit_prob['step'])+1)
+        print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
               + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
-    os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-                    + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+        os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+                  + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
     
-    fig = plt.figure(figsize=(8,8),dpi=250)
-    img = mpimg.imread(best_fit_prob['folder']+'/fake_samples_binarized.png')
-    plt.imshow(img)
-    pp.savefig(fig)
-    plt.close()
+        fig = plt.figure(figsize=(8,8),dpi=250)
+        img = mpimg.imread(best_fit_prob['folder']+'/fake_samples_binarized.png')
+        plt.imshow(img)
+        pp.savefig(fig)
+        plt.close()
         
 
     
 pp = PdfPages(main_folder+'/training_error_summary.pdf')
 num_bins = 28
-# UNIFORM FIRING RATES
-# no refractory period    
-dataset = 'uniform_fr'
-num_classes = 1
-classes_proportion = 'equal' 
-ref_period = -1
 num_samples = 8192#16384
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+option = 4
+all_options = False#True#
+ac_or_prob = 'ac'
+if option==1 or all_options:
+    # UNIFORM FIRING RATES
+    # no refractory period    
+    dataset = 'uniform_fr'
+    num_classes = 1
+    classes_proportion = 'equal' 
+    ref_period = -1
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
+    
+    
+if option==2 or all_options:
+    # refractory period = 2
+    dataset = 'uniform_fr'
+    num_classes = 1
+    classes_proportion = 'equal' 
+    ref_period = 2
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
 
-# refractory period = 2
-dataset = 'uniform_fr'
-num_classes = 1
-classes_proportion = 'equal' 
-ref_period = 2
+if option==3 or all_options:
+    # GAUSSIAN FIRING RATES
+    # no refractory period
+    dataset = 'gaussian_fr'
+    num_classes = 1
+    classes_proportion = 'equal' 
+    ref_period = -1
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
 
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+if option==4 or all_options:
+    # refractory period = 2
+    dataset = 'gaussian_fr'
+    num_classes = 1
+    classes_proportion = 'equal' 
+    ref_period = 2
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
 
+if option==5 or all_options:
+    # 2 classes / refractory period = -1
+    dataset = 'gaussian_fr'
+    num_classes = 2
+    classes_proportion = 'equal' 
+    ref_period = -1
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
+        
+if option==6 or all_options:
+    # 2 classes / refractory period = 2
+    dataset = 'gaussian_fr'
+    num_classes = 2
+    classes_proportion = 'equal' 
+    ref_period = 2
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
 
-# GAUSSIAN FIRING RATES
-# no refractory period
-dataset = 'gaussian_fr'
-num_classes = 1
-classes_proportion = 'equal' 
-ref_period = -1
-
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
-
-
-# refractory period = 2
-dataset = 'gaussian_fr'
-num_classes = 1
-classes_proportion = 'equal' 
-ref_period = 2
-
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
-
-
-# 2 classes / refractory period = -1
-dataset = 'gaussian_fr'
-num_classes = 2
-classes_proportion = 'equal' 
-ref_period = -1
-
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
-
-# 2 classes / refractory period = 2
-dataset = 'gaussian_fr'
-num_classes = 2
-classes_proportion = 'equal' 
-ref_period = 2
-
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
-
-# 2 classes / refractory period = -1 / classes proportion 70-30
-dataset = 'gaussian_fr'
-num_classes = 2
-classes_proportion = '7030' 
-ref_period = -1
-
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
-
-
-# 2 classes / refractory period = 2 / classes proportion 70-30
-num_classes = 2
-classes_proportion = '7030' 
-ref_period = 2
-
-get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp)
+if option==7 or all_options:
+    # 2 classes / refractory period = -1 / classes proportion 70-30
+    dataset = 'gaussian_fr'
+    num_classes = 2
+    classes_proportion = '7030' 
+    ref_period = -1
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
 
 
-pp.close()
+if option==8 or all_options:
+    # 2 classes / refractory period = 2 / classes proportion 70-30
+    num_classes = 2
+    classes_proportion = '7030' 
+    ref_period = 2
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    if not all_options:
+        pp.close()
+
+if all_options:
+    pp.close()

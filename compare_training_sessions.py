03a0c026f0856b344ea045958102c5a90ec9fd74
molano <mmolano0@hotmail.com>
2017-05-11 14:40:00 +0200

    add firing rate parameter

--- compare_training_sessions.py
+++ compare_training_sessions.py
@@ -14,11 +14,11 @@ os.chdir(main_folder)
 import utils
 
 
-def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob):
+def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate):
     folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period) + '_iteration_0'
+  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period)+ '_firing_rate_' + str(firing_rate)  + '_iteration_0'
 
-    title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) 
+    title =   dataset + ' numClasses:' + str(num_classes) + ' propClasses:' + classes_proportion + ' refPeriod:' + str(ref_period) + ' fr:' + str(firing_rate)
     best_fit_ac,best_fit_prob = utils.compare_trainings(main_folder+folder,title)
     fig = plt.figure(figsize=(8,8),dpi=250)
     img = mpimg.imread(main_folder+folder+'/training_error.png')
@@ -36,9 +36,9 @@ def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,n
         iteration = best_fit_ac['folder'][best_fit_ac['folder'].find('iteration')+10:]
         training_stage = int(int(best_fit_ac['epoch'])*num_samples/64+int(best_fit_ac['step'])+1)
         print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-                  + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+                  + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
         os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-                        + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+                        + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
      
         fig = plt.figure(figsize=(8,8),dpi=250)
         img = mpimg.imread(best_fit_ac['folder']+'/fake_samples_binarized.png')
@@ -57,9 +57,9 @@ def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,n
         iteration = best_fit_prob['folder'][best_fit_prob['folder'].find('iteration')+10:]
         training_stage = int(int(best_fit_prob['epoch'])*num_samples/64+int(best_fit_prob['step'])+1)
         print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-              + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+              + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
         os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-                  + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
+                  + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
     
         fig = plt.figure(figsize=(8,8),dpi=250)
         img = mpimg.imread(best_fit_prob['folder']+'/fake_samples_binarized.png')
@@ -73,8 +73,9 @@ pp = PdfPages(main_folder+'/training_error_summary.pdf')
 num_bins = 28
 num_samples = 8192#16384
 option = 4
-all_options = False#True#
+all_options = False
 ac_or_prob = 'ac'
+firing_rate = 0.5
 if option==1 or all_options:
     # UNIFORM FIRING RATES
     # no refractory period    
@@ -82,7 +83,7 @@ if option==1 or all_options:
     num_classes = 1
     classes_proportion = 'equal' 
     ref_period = -1
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
     
@@ -93,7 +94,7 @@ if option==2 or all_options:
     num_classes = 1
     classes_proportion = 'equal' 
     ref_period = 2
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
 
@@ -104,7 +105,7 @@ if option==3 or all_options:
     num_classes = 1
     classes_proportion = 'equal' 
     ref_period = -1
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
 
@@ -114,7 +115,7 @@ if option==4 or all_options:
     num_classes = 1
     classes_proportion = 'equal' 
     ref_period = 2
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
 
@@ -124,7 +125,7 @@ if option==5 or all_options:
     num_classes = 2
     classes_proportion = 'equal' 
     ref_period = -1
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
         
@@ -134,7 +135,7 @@ if option==6 or all_options:
     num_classes = 2
     classes_proportion = 'equal' 
     ref_period = 2
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
 
@@ -144,7 +145,7 @@ if option==7 or all_options:
     num_classes = 2
     classes_proportion = '7030' 
     ref_period = -1
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
 
@@ -154,7 +155,7 @@ if option==8 or all_options:
     num_classes = 2
     classes_proportion = '7030' 
     ref_period = 2
-    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob)
+    get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
     if not all_options:
         pp.close()
 

d67f61d7e10e6f7a944276e59c7d6dc1c91cec0d
molano <mmolano0@hotmail.com>
2017-05-16 18:39:13 +0200

    add option of analysing kayser's data

--- compare_training_sessions.py
+++ compare_training_sessions.py
@@ -14,12 +14,19 @@ os.chdir(main_folder)
 import utils
 
 
-def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate):
+def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate,option=None,neuron=None):
     folder_final_figures = main_folder + 'best fits'
-    folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
-  '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period)+ '_firing_rate_' + str(firing_rate)  + '_iteration_0'
-
-    title =   dataset + ' numSamp ' + str(num_samples) + ' numClass ' + str(num_classes) + ' propClass ' + classes_proportion + ' refPer ' + str(ref_period) + ' fr ' + str(firing_rate)
+    
+    if option==9:
+        folder = 'samples' + neuron + '_num_bins_' + str(num_bins) + '_iteration_0'
+        title =   neuron + ' numSamp ' + str(num_samples) 
+    else:
+        folder = 'samples_dataset_' + dataset + '_num_classes_' + str(num_classes) + '_propClasses_' + classes_proportion + \
+        '_num_samples_' + str(num_samples) + '_num_bins_' + str(num_bins) + '_ref_period_' + str(ref_period)+ '_firing_rate_' + str(firing_rate)  + '_iteration_0'
+        
+        title =   dataset + ' numSamp ' + str(num_samples) + ' numClass ' + str(num_classes) + ' propClass ' + classes_proportion + ' refPer ' + str(ref_period) + ' fr ' + str(firing_rate)
+    
+    
     best_fit_ac,best_fit_prob = utils.compare_trainings(main_folder+folder,title)
     iteration = best_fit_ac['folder'][best_fit_ac['folder'].find('iteration')+10:]
     
@@ -29,52 +36,41 @@ def get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,n
     pp.savefig(fig)
     plt.close()
     
-    
     #plot best ac fit
-    fig = utils.plot_best_fit(best_fit_ac,folder_final_figures+'/'+title+'iter'+iteration+'best_of_all_ac_fit',best_fit_ac['ac_error'],best_fit_ac['spk_mean_error'],'')
+    if option==3:
+        #for the gaussian, no refractory period and 1 class case we will also plot the numerical probability of the samples obtained from 500000 simulated samples
+        fig = utils.plot_best_fit(best_fit_ac,folder_final_figures+'/'+title+'iter'+iteration+'best_of_all_ac_fit',best_fit_ac['ac_error'],best_fit_ac['spk_mean_error'],'',\
+                                  num_classes,num_samples,num_bins, firing_rate,dataset,classes_proportion)
+    else:
+        fig = utils.plot_best_fit(best_fit_ac,folder_final_figures+'/'+title+'iter'+iteration+'best_of_all_ac_fit',best_fit_ac['ac_error'],best_fit_ac['spk_mean_error'],'')
+        
     pp.savefig(fig)
     plt.close()
-    if ac_or_prob=='ac':
-        #now I want to get samples from the best ac fit
-        training_stage = int(int(best_fit_ac['epoch'])*num_samples/64+int(best_fit_ac['step'])+1)
-        print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-                  + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
-        os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-                        + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
-     
-        fig = plt.figure(figsize=(8,8),dpi=250)
-        img = mpimg.imread(best_fit_ac['folder']+'/fake_samples.png')
-        plt.imshow(img)
-        pp.savefig(fig)
-        plt.close()
+    
+    #now I want to get samples from the best ac fit
+    training_stage = int(int(best_fit_ac['epoch'])*num_samples/64+int(best_fit_ac['step'])+1) 
+    if option==9:
+        command = 'python3.5 main.py --dataset kayser_data ' + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage)
+    else:
+        command = 'python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
+          + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage)
+    
+    print(command)
+    os.system(command)
+    
+    fig = plt.figure(figsize=(8,8),dpi=250)
+    img = mpimg.imread(best_fit_ac['folder']+'/fake_samples.png')
+    plt.imshow(img)
+    pp.savefig(fig)
+    plt.close()
     
     
        
-    #plot best prob fit
-#    iteration = best_fit_prob['folder'][best_fit_prob['folder'].find('iteration')+10:]
-#    fig = utils.plot_best_fit(best_fit_prob,folder_final_figures+'/'+title+'iter'+iteration+'best_of_all_prob_fit',best_fit_prob['ac_error'],best_fit_prob['spk_mean_error'],'')
-#    pp.savefig(fig)
-#    plt.close()
-#    if ac_or_prob=='prob':   
-#        #now I want to get samples from the best prob fit
-#        training_stage = int(int(best_fit_prob['epoch'])*num_samples/64+int(best_fit_prob['step'])+1)
-#        print('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-#              + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
-#        os.system('python3.5 main.py --dataset ' + dataset + ' --num_classes=' + str(num_classes) + ' --classes_proportion ' + classes_proportion + ' --ref_period=' + str(ref_period)\
-#                  + ' --firing_rate=' + str(firing_rate) + ' --epoch=50 --num_samples=' + str(num_samples) + ' --iteration ' + iteration + ' --training_stage=' + str(training_stage))
-#    
-#        fig = plt.figure(figsize=(8,8),dpi=250)
-#        img = mpimg.imread(best_fit_prob['folder']+'/fake_samples.png')
-#        plt.imshow(img)
-#        pp.savefig(fig)
-#        plt.close()
-        
-
+ 
 def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=8192,num_bins=28):   
     pp = PdfPages(main_folder+'/training_error_summary.pdf')
     
     #16384
-    ac_or_prob = 'ac'
     if option==1 or all_options:
         # UNIFORM FIRING RATES
         # no refractory period    
@@ -82,7 +78,7 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 1
         classes_proportion = 'equal' 
         ref_period = -1
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate)
         if not all_options:
             pp.close()
             
@@ -93,7 +89,7 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 1
         classes_proportion = 'equal' 
         ref_period = 2
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate)
         if not all_options:
             pp.close()
 
@@ -104,7 +100,7 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 1
         classes_proportion = 'equal' 
         ref_period = -1
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate,option)
         if not all_options:
             pp.close()
 
@@ -114,7 +110,7 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 1
         classes_proportion = 'equal' 
         ref_period = 2
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate)
         if not all_options:
             pp.close()
 
@@ -124,7 +120,7 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 2
         classes_proportion = 'equal' 
         ref_period = -1
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate)
         if not all_options:
             pp.close()
         
@@ -134,7 +130,7 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 2
         classes_proportion = 'equal' 
         ref_period = 2
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate)
         if not all_options:
             pp.close()
 
@@ -144,7 +140,7 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 2
         classes_proportion = '7030' 
         ref_period = -1
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate)
         if not all_options:
             pp.close()
 
@@ -154,17 +150,29 @@ def select_experiment(all_options=False,option=3,firing_rate=0.5,num_samples=819
         num_classes = 2
         classes_proportion = '7030' 
         ref_period = 2
-        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,ac_or_prob,firing_rate)
+        get_figures(main_folder,dataset,num_classes,classes_proportion,num_samples,num_bins,ref_period,pp,firing_rate)
         if not all_options:
             pp.close()
-
+            
+    if option==9:
+        num_samples = 21420#19992 #
+        neuron = 'AuditoryDataM03.Jg_longnat3_sites2_neuron_4_noise_1.mat'#'AuditoryDataM03.Jf_longnat3_sites3_neuron_3_noise_1.mat' #
+        get_figures(main_folder,0,0,0,num_samples,num_bins,0,pp,firing_rate,option,neuron)
+        if not all_options:
+            pp.close()
+        
+    
+    
     if all_options:
         pp.close()
 
 
 #select_experiment(False,3,num_samples=2048)
 #select_experiment(False,4,num_samples=2048)
-select_experiment(False,5)
-select_experiment(False,7)
-select_experiment(False,3)
-select_experiment(False,4)
+#select_experiment(False,3)
+#select_experiment(False,4)
+#select_experiment(False,5)
+#select_experiment(False,7)
+#select_experiment(False,9)
+select_experiment(False,3,num_samples=1024)
+select_experiment(False,4,num_samples=1024)

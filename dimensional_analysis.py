2acde9664b3d1ed1938304aec447d33457bba30b
Eugenio Piasini <eugenio.piasini@gmail.com>
2017-05-18 18:12:05 +0200

    First attempt at dimensional analysis with JS distance.

---
+++ dimensional_analysis.py
@@ -0,0 +1,67 @@
+#!/env/python
+import os
+
+from subprocess import call
+import numpy as np
+
+import utils
+
+folder_name = "samples_dataset_gaussian_fr_num_classes_1_propClasses_equal_num_samples_2048_num_bins_28_ref_period_-1_firing_rate_0.5_iteration_18"
+
+def generate_fake():
+    call(["python3.5", "main.py", "--dataset", "gaussian_fr", "--num_classes=1", "--classes_proportion", "equal", "--ref_period=-1", "--firing_rate=0.5", "--epoch=50", "--num_samples=2048", "--iteration", "18", "--training_stage=801"])
+    temp = np.load(os.path.join(folder_name, "fake_samples.npz"))
+    return temp['fake_samples'].astype(np.bool)
+
+def generate_real():
+    return(utils.get_more_real_samples_gaussian_no_refrPer(num_samples=2048).astype(np.bool))
+
+def frequency_table(a):
+    num_samples = a.shape[0]
+    b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
+    unique_b, idx, counts = np.unique(b, return_index=True, return_counts=True)
+    unique_a = a[idx]
+    frequencies = counts/num_samples
+    return dict([(key.tostring(), value) for (key, value) in zip(unique_a, frequencies)])
+    
+def jensen_shannon_distance(p, q):
+    for key in q.keys() - p.keys():
+        p[key] = 0
+    for key in p.keys() - q.keys():
+        q[key] = 0
+    
+    # note: this can obviously be optimised by getting rid of the for loop
+    divergence = 0
+    for key in p.keys():
+        px = p[key]
+        qx = q[key]
+        mx = (px+qx)/2
+        divergence += (kl_term(px,mx)+kl_term(qx,mx))/2
+    
+    return np.sqrt(divergence)
+        
+def kl_term(px, qx):
+    if px>0:
+        return px * np.log2(px/qx)
+    else:
+        return 0
+    
+def full_distance_matrix(n_real, n_fake):
+    real = [frequency_table(generate_real()) for each in range(n_real)]
+    fake = [frequency_table(generate_fake()) for each in range(n_fake)]
+    return pairwise_distances(real + fake)
+
+    
+def pairwise_distances(ps):
+    n_points = len(ps)
+    distances = np.zeros(int(n_points*(n_points-1)/2))
+    idx = 0
+    for i in range(n_points):
+        for j in range(i+1, n_points):
+            distances[idx] = jensen_shannon_distance(ps[i], ps[j])
+            idx+=1
+    return distances
+    
+
+
+    
